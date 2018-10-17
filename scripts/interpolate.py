6fa84b854af3f6670a8627b5a7a8fd26fe8ce1fa
JosepRIvaille <josep.de.cid@gmail.com>
2018-10-17 20:14:02 +0200

    Add a snake

--- scripts/interpolate.py
+++ scripts/interpolate.py
@@ -2,22 +2,34 @@ import numpy as np
 from scipy import interpolate
 
 
-def generate_data():
+def preprocess_data():
     """
     Generate data to feed the interpolator
     :return: independent variable, latitude and longitude
     """
-    data = [[41.1, 2.1, 10], [41.1, 2.5, 15]]
+    data = [[45.849, 15.836, 50], [45.74, 16.0809, 20]]
     data.sort(key=lambda el: el[-1])
     data = np.array(data)
     return data[:, -1], data[:, 0], data[:, 1]
 
 
-def interpolate_data():
-    cont, lat, lng = generate_data()
-    interpolator = interpolate.Rbf(lat, lng, cont)
-    print(interpolator(41.1, 2.3))
+def interpolate_data(min_lat, min_lng, max_lat, max_lng, steps):
+    weights, lats, longs = preprocess_data()
+    interpolator = interpolate.Rbf(lats, longs, weights)
 
+    y = np.linspace(min_lat, max_lat, num=steps)
+    x = np.linspace(min_lng, max_lng, num=steps)
+    grid_x, grid_y = np.meshgrid(y, x, sparse=False)
 
-if __name__ == '__main__':
-    interpolate_data()
+    response = []
+    for i in range(len(grid_x)):
+        for j in range(len(grid_x[0])):
+            lat = grid_x[i, j]
+            lng = grid_y[i, j]
+            value = interpolator(lat, lng)
+            response.append({
+                'lat': lat,
+                'lng': lng,
+                'weight': value.tolist()
+            })
+    return response
