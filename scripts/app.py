fd0299fccf6238d25dcd5e724c023f2b4f326bcb
JosepRIvaille <josep.de.cid@gmail.com>
2018-10-17 20:14:02 +0200

    Add python

---
+++ scripts/app.py
@@ -0,0 +1,22 @@
+from flask import Flask, request, jsonify
+from interpolate import interpolate_data
+
+app = Flask(__name__)
+
+
+@app.route('/', methods=['GET'])
+def get_data():
+    min_lat = request.args.get('min_lat', type=float)
+    min_lng = request.args.get('min_lng', type=float)
+    max_lat = request.args.get('max_lat', type=float)
+    max_lng = request.args.get('max_lng', type=float)
+    if None in [min_lat, min_lng, max_lat, max_lng]:
+        return 'Missing parameters', 400
+
+    steps = request.args.get('steps', 10)
+    response = interpolate_data(min_lat, min_lng, max_lat, max_lng, steps)
+    return jsonify(response)
+
+
+if __name__ == '__main__':
+    app.run(port=8080)
