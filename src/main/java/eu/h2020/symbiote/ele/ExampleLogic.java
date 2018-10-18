c2901c28abad247fe69124f3b7b9407777f61a48
Gonzalo Recio <gonzalo.recio@est.fib.upc.edu>
2018-10-18 11:09:18 +0200

    Backend 2.0

--- src/main/java/eu/h2020/symbiote/ele/ExampleLogic.java
+++ src/main/java/eu/h2020/symbiote/ele/ExampleLogic.java
@@ -2,6 +2,7 @@ package eu.h2020.symbiote.ele;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import eu.h2020.symbiote.core.ci.QueryResourceResult;
 import eu.h2020.symbiote.core.internal.CoreQueryRequest;
 import eu.h2020.symbiote.enabler.messaging.model.*;
 import eu.h2020.symbiote.enablerlogic.EnablerLogic;
@@ -9,6 +10,7 @@ import eu.h2020.symbiote.enablerlogic.ProcessingLogic;
 import eu.h2020.symbiote.enablerlogic.messaging.RegistrationHandlerClientService;
 import eu.h2020.symbiote.enablerlogic.messaging.properties.EnablerLogicProperties;
 import eu.h2020.symbiote.model.cim.Observation;
+import eu.h2020.symbiote.model.cim.ObservationValue;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -16,6 +18,7 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 import java.util.Optional;
 
@@ -40,31 +43,72 @@ public class ExampleLogic implements ProcessingLogic {
         this.enablerLogic = enablerLogic;
     }
 
-    String queryAir() {
-        Optional<PlatformProxyResourceInfo> airResources = getAirResources();
+    List<String> queryAir(String prop) {
+        List<Optional<PlatformProxyResourceInfo>> airResources = getAirResources(prop);
+        List<String> observations = new ArrayList<>();
+        for (Optional<PlatformProxyResourceInfo> airResource : airResources) {
+            observations.add(getObservation(airResource));
+        }
+        return observations;
+    }
+
+    private String getObservation(Optional<PlatformProxyResourceInfo> airResource) {
         final String[] value = new String[1];
-        airResources.ifPresent(resource -> {
+
+        airResource.ifPresent(resource -> {
             PlatformProxyTaskInfo taskInfo = new PlatformProxyTaskInfo();
             taskInfo.setTaskId("ZagrebResource");
             List<PlatformProxyResourceInfo> myList = new ArrayList<>();
             myList.add(resource);
             taskInfo.setResources(myList);
             LOG.info("Getting resources from {}", resource.getResourceId());
-            List<Observation> observations = enablerLogic.readResource(taskInfo).getObservations();
-            System.out.println(observations.size());
-            observations.forEach(observation -> System.out.println(observation.getObsValues()));
-            value[0] = observations.get(observations.size() -1).getObsValues().get(0).getValue();
+            EnablerLogicDataAppearedMessage enablerLogicDataAppearedMessage = enablerLogic.readResource(taskInfo);
+            if (enablerLogicDataAppearedMessage != null) {
+                List<Observation> observations = enablerLogicDataAppearedMessage.getObservations();
+                System.out.println(observations.size());
+                observations.forEach(observation -> System.out.println(observation.getObsValues()));
+                if (!observations.isEmpty()) {
+                    List<ObservationValue> obsValues = observations.get(observations.size() - 1).getObsValues();
+                    if (!obsValues.isEmpty())
+                        value[0] = obsValues.get(0).getValue();
+                }
+            }
         });
         return value[0];
     }
 
-    private Optional<PlatformProxyResourceInfo> getAirResources() {
+    private List<Optional<PlatformProxyResourceInfo> > getAirResources(String prop) {
+        //List<String> properties = Arrays.asList("nitrogen dioxide concentration", "ozone concentration", "carbon monoxide concentration", "Particulate matter <10um (aerosol) concentration");
+        //List<ResourceManagerTaskInfoResponse> tasks = new ArrayList<>();
+        //for (String prop : properties) {
+        //    tasks.add(getResourceManagerTaskInfoResponses(prop).get(0));
+        //}
+        List<Optional<PlatformProxyResourceInfo> > resources = new ArrayList<>();
+        ResourceManagerTaskInfoResponse task = getResourceManagerTaskInfoResponses(prop).get(0);
+        // for (ResourceManagerTaskInfoResponse task : tasks) {
+            List<QueryResourceResult> resourceDescriptions = task.getResourceDescriptions();
+            for (QueryResourceResult resource : resourceDescriptions) {
+                String resourceId = resource.getId();
+                String accessURL = task.getResourceUrls().get(resourceId);
+
+                PlatformProxyResourceInfo info = new PlatformProxyResourceInfo();
+                info.setAccessURL(accessURL);
+                info.setResourceId(resourceId);
+                resources.add(Optional.of(info));
+            }
+        //}
+        return resources;
+
+    }
+
+    private List<ResourceManagerTaskInfoResponse> getResourceManagerTaskInfoResponses(String prop) {
         CoreQueryRequest coreQueryRequest = new CoreQueryRequest();
         coreQueryRequest.setLocation_name("*Zagreb*");
-        coreQueryRequest.setResource_type("StationarySensor");
+        coreQueryRequest.setResource_type("StationarySensor");;
+        coreQueryRequest.setObserved_property(Arrays.asList(prop));
 
         ResourceManagerTaskInfoRequest request = new ResourceManagerTaskInfoRequest(
-                "airCondition", 1, 1, coreQueryRequest,
+                "airCondition", 1, 10, coreQueryRequest,
                 null, //"P0000-00-00T00:01:00",
                 false, null, false, props.getEnablerName(), null);
 
@@ -77,18 +121,9 @@ public class ExampleLogic implements ProcessingLogic {
         }
         if(response.getStatus() != ResourceManagerTasksStatus.SUCCESS) {
             LOG.warn("Did not found aircondition actuator!!!");
-            return Optional.empty();
         }
 
-        ResourceManagerTaskInfoResponse resourceManagerTaskInfoResponse = response.getTasks().get(0);
-        String resourceId = resourceManagerTaskInfoResponse.getResourceDescriptions().get(0).getId();
-        String accessURL = resourceManagerTaskInfoResponse.getResourceUrls().get(resourceId);
-
-        PlatformProxyResourceInfo info = new PlatformProxyResourceInfo();
-        info.setAccessURL(accessURL);
-        info.setResourceId(resourceId);
-        return Optional.of(info);
-
+        return response.getTasks();
     }
 
     @Override
--- src/main/java/eu/h2020/symbiote/ele/HackaTownController.java
+++ src/main/java/eu/h2020/symbiote/ele/HackaTownController.java
@@ -5,6 +5,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 @RestController
@@ -13,11 +14,14 @@ public class HackaTownController {
     @Autowired
     ExampleLogic exampleLogic;
 
-    @GetMapping("/sex")
-    public List<String> getSex() {
-        List<String> sexs = new ArrayList<>();
-        sexs.add(exampleLogic.queryAir());
-        return sexs;
+    @GetMapping("/getData")
+    public List<List<String> > getData() {
+        List<String> properties = Arrays.asList("nitrogen dioxide concentration", "ozone concentration", "carbon monoxide concentration", "Particulate matter <10um (aerosol) concentration");
+        List<List<String> > result = new ArrayList<>(new ArrayList<>());
+        for (String prop : properties) {
+            result.add(exampleLogic.queryAir(prop));
+        }
+        return result;
     }
 
 }
