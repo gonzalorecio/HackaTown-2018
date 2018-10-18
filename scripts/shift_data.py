f9e3d0b73b731e08bee0243efee00033366d70ba
JosepRIvaille <josep.de.cid@gmail.com>
2018-10-18 14:11:45 +0200

    Things

--- scripts/shift_data.py
+++ scripts/shift_data.py
@@ -4,11 +4,11 @@ from datetime import datetime
 import numpy as np
 
 dist = np.linspace(0, 23, 24)
-dist = list(map(np.sin, dist))
+dist = list(map(lambda d: np.sin(d/10), dist))
 
 
 def shift(data, offset):
     hour = datetime.now().hour
     hour += offset
     hour %= 24
-    return [data[0], data[1], data[2] + random() * dist[hour], data[3] + random() * dist[hour]]
+    return [data[0], data[1], data[2] + random()*5 * dist[hour], data[3] + random()*5 * dist[hour]]
